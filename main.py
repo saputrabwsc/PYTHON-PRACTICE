e938ce50afef0f20c61db0f4e308f7d43535ac20
saputrabwsc <159851389+saputrabwsc@users.noreply.github.com>
2024-05-02 12:23:55 +1200

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+print("hello")
+# Ask the user their name and save it
+name = input("what's your name")
+# Greet the user and introduce the quiz
+print("welcome to the quiz", name)
+# Ask the user a question
+input("what is 1+1")
+# tell them the correct answer
+print("1+1 is 2")
+# End the quiz
+input("thank you for playing")
